48296ab23b302f2197291e4677e28465e95f269d
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2015-12-06 12:13:34 +0000

    [RELEASE] 7.0.0 beta5
    git-svn-id: https://svn.code.sf.net/p/ultradefrag/code/trunk@4292 297d8b2e-af85-426a-ac7c-16af7146bd1b

--- src/include/version.h
+++ src/include/version.h
@@ -1,6 +1,6 @@
 #define VERSION 7,0,0,0 
 #define VERSION2 "7, 0, 0, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 7.0.0 beta4" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 7.0.0 beta4 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 7.0.0 beta4" 
-#define wxUD_ABOUT_VERSION "7.0.0 beta4" 
+#define VERSIONINTITLE "UltraDefrag 7.0.0 beta5" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 7.0.0 beta5 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 7.0.0 beta5" 
+#define wxUD_ABOUT_VERSION "7.0.0 beta5" 
